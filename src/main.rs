b5690e2dfc722fa4f7efc3f5d3288216ecf7db00
Alex DeFreese <alexdefreese@gmail.com>
2021-05-04 21:13:38 -0400

    Baseline setup for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    set: i32,
+    challenge: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    let set = args.set;
+    let challenge = args.challenge;
+
+    println!("Running Set {} Challenge {}", set, challenge);
+
+    match set {
+        1 => {
+            match challenge {
+                _ => println!("Nothing available Set Challenge {}", challenge),
+            }
+        },
+        _ => { println!("No challenges available from set {}", set); },
+    }
 }
